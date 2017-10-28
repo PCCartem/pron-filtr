bada60ed0093dfa8b6c213c2c84140baa2b80a5b
Artem Vedernikov <freeflight.pro@gmail.com>
2017-10-29 00:52:39 +0500

    pron-parser was created

---
+++ app.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+require "vendor/autoload.php";
+require_once "par/ActiveRecord.php";
+
+\ActiveRecord\Config::initialize(function($cfg)
+ {
+     $cfg->set_model_directory('C:\OpenServer\domains\pf.loc\models');
+     $cfg->set_connections(array(
+         'development' => 'mysql://root:@localhost/pf'));
+ });
+$app = new \App\Brut();
+$app->getCatsUrls();
+//echo "Верный пароль".$app->lync;
